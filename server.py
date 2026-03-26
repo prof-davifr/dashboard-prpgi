1ae718663914f29918ee9cb7909b8e4615a8d4ce
Davi <prof.davifr@gmail.com>
2026-03-26 13:13:29 -0300

    feat: Dynamically discover Excel data files and their year ranges, update period filter options, and add a server endpoint to list data files.

--- server.py
+++ server.py
@@ -5,9 +5,22 @@ import os
 PORT = 8000
 DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
+import json
+
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def do_GET(self):
+        if self.path == '/api/files':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            dados_path = os.path.join(DIRECTORY, 'dados')
+            files = [f for f in os.listdir(dados_path) if f.endswith('.xls')]
+            self.wfile.write(json.dumps(files).encode())
+        else:
+            super().do_GET()
 
 if __name__ == "__main__":
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
